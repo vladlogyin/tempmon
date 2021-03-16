4d1325f6e3b22aac375c631532757f5c14dfea22
vladlogyin <vladlogyin@gmail.com>
2021-03-16 14:32:21 +0000

    Node Widget WIP

--- tempmon-gtk.cpp
+++ tempmon-gtk.cpp
@@ -1,14 +1,47 @@
 #include <tempmon-gtk.h>
-
+//NodeWidget nw;
 TempmonWindow::TempmonWindow()
 {
   set_border_width(10);
-  button.signal_clicked().connect(sigc::mem_fun(*this,&TempmonWindow::onbutton));
-  add(button);
-  button.show();
+  //button.signal_clicked().connect(sigc::mem_fun(*this,&TempmonWindow::onbutton));
+  //nodetest = NodeWidget();
+  //Gtk::Box b;
+  //add(hbox);
+  //nw
+  //hbox.pack_end(nw);
+  this->show_all();
 }
 
 void TempmonWindow::onbutton()
 {
   std::cout<<"henlo"<<std::endl;
+}
+
+NodeWidget::NodeWidget() : Gtk::Box()
+{
+  //this->sen = Sensor("henlo:",SensorDisplay());
+  //this->sen.display.setInt();
+  //this->pack_start(sen);
+}
+
+NodeWidget::NodeWidget(std::shared_ptr<Client> c)
+{
+  this->id = c->UID;
+  //this->
+}
+
+Sensor::Sensor(std::string name, SensorDisplay display) : Gtk::Box()
+{
+  this->name = Gtk::Label(name);
+  this->display = std::move(display);//StringDisplay("worl");//std::move(display);
+  this->pack_start(this->name);
+  //this->pack_start(this->display);
+}
+
+StringDisplay::StringDisplay(std::string s) : SensorDisplay()
+{
+  //this->value = Gtk::Label("asd");
+  //this->set_label("asdf");
+  //this->set_text(s);
+  //show_all();
 }
--- tempmon-gtk.h
+++ tempmon-gtk.h
@@ -1,17 +1,58 @@
 #include <iostream>
 #include <string>
 
+#include <client.h>
+
 #include <gtkmm-3.0/gtkmm.h>
 
-class TempmonWindow : public Gtk::Window
+class SensorDisplay : public Gtk::Container
 {
 public:
-  TempmonWindow();
-  void onbutton();
-//  void addNode(std::shared_ptr<Client> );
-//  void deleteNode(std::shared_ptr<Client>);
-  Gtk::Button button;
-  Gtk::Box hbox;
+  Gtk::Label label = Gtk::Label("asdfg");
+  SensorDisplay() : Gtk::Container()
+  {
+    this->add(label);
+  };
+  std::string stringvalue="";
+  int intvalue=0;
+  float floatvalue=0;
+  void setString(std::string s)
+  {
+    stringvalue = s;
+    intvalue=-1;
+    floatvalue = 0;
+  }
+  void setInt(int i)
+  {
+    stringvalue = std::to_string(i);
+    intvalue = i;
+    floatvalue = i;
+  }
+  void setFloat(float f)
+  {
+    stringvalue = std::to_string(f);
+    intvalue = (int)f;
+    floatvalue = f;
+  }
+};
+
+class StringDisplay : public SensorDisplay
+{
+public:
+  Gtk::Label value;
+  StringDisplay(){};
+  StringDisplay(std::string);
+  //void set_manage(){Gtk::Label::set_manage();}
+  //void destroy_notify_(){Gtk::Label::destroy_notify_();}
+};
+
+class Sensor : public Gtk::Box
+{
+public:
+  Sensor(){};
+  Sensor(std::string, SensorDisplay);
+  Gtk::Label name;
+  SensorDisplay display;
 };
 
 class NodeWidget : public Gtk::Box
@@ -19,11 +60,23 @@ class NodeWidget : public Gtk::Box
 public:
   uint32_t id=0;
   NodeWidget();
+  NodeWidget(std::shared_ptr<Client>);
+  //Sensor sen;
+  Gtk::Label title;
+  
 };
 
-class NameValuePair : public Gtk::Box
+class TempmonWindow : public Gtk::Window
 {
 public:
-  NameValuePair(std::string, std::string);
-  std::string name,value;
+  TempmonWindow();
+  void onbutton();
+  void addNode(std::shared_ptr<Client>);
+  void deleteNode(std::shared_ptr<Client>);
+  void updateNode(std::shared_ptr<Client>);
+  //Gtk::Button button;
+  //NodeWidget nw;
+  //Gtk::Box hbox;
 };
+
+
